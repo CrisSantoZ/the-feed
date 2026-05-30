9c7b253bcabdbbacb9ddedc461c3e56f8c0cf80f
ChrisDammicco <christiandammicco@gmail.com>
2026-05-30 03:20:42 -0300

    fix: desativa renderizacao de SVGs nos paises (causava zoom)

--- client/js/mundo/mapaPais.js
+++ client/js/mundo/mapaPais.js
@@ -1,4 +1,4 @@
-import { renderizarMapaPaisSVG, initMapaPaisSVG } from './mapaPaisSVG.js';
+import { renderizarMapaPaisSVG } from './mapaPaisSVG.js';
 
 export async function renderizarMapaPais(paisNome) {
     const paises = window.paisesDataGlobal || [];
@@ -31,7 +31,7 @@ export async function renderizarMapaPais(paisNome) {
         </div>
     `).join('');
 
-    const svgHtml = paisNome !== 'Brasil' ? await renderizarMapaPaisSVG(paisNome) : null;
+    const svgHtml = null;
 
     return `
         <div class="mapa-container" style="background:#030407;">
@@ -77,5 +77,5 @@ export async function renderizarMapaPais(paisNome) {
 }
 
 export function afterRenderMapa(paisNome) {
-    if (paisNome !== 'Brasil') initMapaPaisSVG(paisNome);
+    // Mapas SVG desativados - apenas navegacao por cards
 }
